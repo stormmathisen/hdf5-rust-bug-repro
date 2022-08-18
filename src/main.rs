7bd47dfef14c683b9ec2e03ca6ba730904f379df
Storm <storm.mathisen@stfc.ac.uk>
2022-08-18 19:59:36 +0100

    Comments

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,19 @@ const GROUPS: u64 = 400000;
 
 fn main() {
     let fake_data: [u16; 512] = [255; 512];
-
+    //Create file
     let hdffile = hdf5::File::create("AFile.h5").unwrap();
+    
         
     for i in 0..GROUPS
     {
         let groupname = format!("{}", i);
-        
+        //Create group
         let hdfgroup = hdffile.create_group(&groupname).unwrap();
+        
 
         for i in 0..PER_GROUP {
+            //Write PER_GROUP 8kb arrays as individual datasets
             let ds_name = format!("{}", i);
 
             hdfgroup
@@ -25,9 +28,12 @@ fn main() {
                 .unwrap();
         }
     }
+    //Close the HDF file
     hdffile.close().unwrap();
     println!("Finished!");
     loop {
+        //Hang about until quit.
+        //Memory useage will remain high, would expect dropping the file to stop that
         std::hint::spin_loop();
     }
 
