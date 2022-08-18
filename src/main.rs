d8e8b63bcdc2e1982f769aa1f31f424df965689f
Storm <storm.mathisen@stfc.ac.uk>
2022-08-18 19:22:06 +0100

    Bug reproduced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use hdf5;
+
+const PER_GROUP: u64 = 10;
+const GROUPS: u64 = 400000;
+
+
 fn main() {
-    println!("Hello, world!");
+    let fake_data: [u16; 512] = [255; 512];
+
+    let hdffile = hdf5::File::create("AFile.h5").unwrap();
+        
+    for i in 0..GROUPS
+    {
+        let groupname = format!("{}", i);
+        
+        let hdfgroup = hdffile.create_group(&groupname).unwrap();
+
+        for i in 0..PER_GROUP {
+            let ds_name = format!("{}", i);
+
+            hdfgroup
+                .new_dataset_builder()
+                .with_data(&fake_data)
+                .create(&*ds_name)
+                .unwrap();
+        }
+    }
+    hdffile.close().unwrap();
+    println!("Finished!");
+    loop {
+        std::hint::spin_loop();
+    }
+
 }
